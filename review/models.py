cd440ae8c9557cc8e19bd532779b20a58e1da1ca
Bas Nawaphat <s5801012610075@email.kmutnb.ac.th>
2019-02-20 19:15:54 +0700

    Add 2 models for store data of restanrant and review post

--- review/models.py
+++ review/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=10)
+    rating = models.DecimalField(default=0, max_digits=2, decimal_places=1)
+
+    def __str__(self):
+        return self.name   
+
+class ReviewPost(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review_topic = models.CharField(max_length=200)
+    review_datail = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# class FoodCategory(models.Model):
+#     category = models.CharField(max_length=200)
+
+
+
