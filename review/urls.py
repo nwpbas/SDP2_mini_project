861a8046b10a08cbebb0366f0a9474f001005b5d
Bas Nawaphat <nwpaprbas@gmail.com>
2019-02-21 12:26:46 +0700

    Add button for create review post
    Update index template to be able to use variable tag to show value in model object
    
    Add view function for render form add restanrant template and
    
    Add view function for insert form data to model
    
    Add view function for render detail of restaurant

--- review/urls.py
+++ review/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'review'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('form_add_rest/', views.formAddRestaurant, name='form_add_restaurant'),
+    path('add_rest/', views.addRestaurant, name='add_restaurant'),
+    path('restaurant/<int:id_rest>/', views.detailRest, name='detail'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+from .models import Restaurant, ReviewPost
 
 def index(request):
-    return render(request,"review/index.html")
+    restaurant_obj_list = Restaurant.objects.all()
+    context = {"restaurant_obj_list":restaurant_obj_list,}
+    print(context)
+    return render(request,"review/index.html",context)
+
+def formAddRestaurant(request):
+    return render(request,'review/add_restaurant.html')
+
+def addRestaurant(request):
+    name = request.POST['name']
+    address = request.POST['address']
+    phone_number = request.POST['phone_number']
+    Restaurant.objects.create(name=name, address=address, phone_number=phone_number)
+    return HttpResponseRedirect( reverse('review:index') )
+
+def detailRest(request, id):
+    restaurant_obj = Restaurant.objects.get(pk=id)
+    context = {"restaurant_obj":restaurant_obj,}
+    print(context)
+    return render(request,"review/index.html",context)
+
