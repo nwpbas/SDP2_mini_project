67e2784d1866bc069f6f2622110a623fdb3be40b
Bas Nawaphat <nwpaprbas@gmail.com>
2019-03-03 00:44:22 +0700

    Add view function for render writing review's template
    Add view function for add review
    
    Edit the display of review post to be able to show from the django model.

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('form_add_rest/', views.formAddRestaurant, name='form_add_restaurant'),
     path('add_rest/', views.addRestaurant, name='add_restaurant'),
     path('restaurant/<int:id_rest>/', views.detailRest, name='detail'),
+    path('restaurant/<int:id_rest>/write_reivew', views.formWriteReview, name='form_write_review'),
+    path('restaurant/add_review', views.addReview, name='add_review'),
 ]
--- review/views.py
+++ review/views.py
@@ -20,9 +20,27 @@ def addRestaurant(request):
     Restaurant.objects.create(name=name, address=address, phone_number=phone_number)
     return HttpResponseRedirect( reverse('review:index') )
 
-def detailRest(request, id):
-    restaurant_obj = Restaurant.objects.get(pk=id)
-    context = {"restaurant_obj":restaurant_obj,}
-    print(context)
-    return render(request,"review/index.html",context)
+def detailRest(request, id_rest):
+    restaurant_obj = Restaurant.objects.get(pk=id_rest)
+    all_review_obj = ReviewPost.objects.filter(restaurant_id=id_rest).order_by('-timestamp')
+    context = {'restaurant_obj':restaurant_obj,
+                'all_review_obj':all_review_obj,}  
+    return render(request,"review/detail.html",context)
+
+def formWriteReview(request, id_rest):
+    restaurant_obj = Restaurant.objects.get(pk=id_rest)
+    context = {'restaurant_obj':restaurant_obj,} 
+    return render(request,'review/write_review.html',context)
+
+def addReview(request):
+    print(request.POST)
+    restaurant_obj = Restaurant.objects.get(pk=request.POST['restaurant_id'])
+    r_topic = request.POST['review_topic'] 
+    r_detail = request.POST['review_detail']
+    rating = request.POST['rating']
+    restaurant_obj.reviewpost_set.create(review_topic=r_topic, review_datail=r_detail, rating=rating)
+    return HttpResponseRedirect( reverse('review:detail', args=(request.POST['restaurant_id'],)) )
+
+
+
 
